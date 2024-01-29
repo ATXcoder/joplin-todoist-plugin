14d23fe5351b6062eafbdf18a9c65ac10a1007e0
Tom <tsloan82@gmail.com>
2024-01-29 14:27:17 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
-# Joplin Plugin
+# Joplin to Todist
 
-This is your new Joplin plugin. It is suggested that you use this README file to document your plugin.
+This [Joplin](#) plugin allows you to create a [Todoist](#) task directly from Joplin.
 
-For information on how to build or publish the plugin, please see [GENERATOR_DOC.md](./GENERATOR_DOC.md)
+I use Todoist to manage all my personal and professional tasks and Joplin to store all my notes. I needed a way to quickly create tasks in Todoist directly from Joplin while also tieing the task to the current Joplin note. I wrote this plugin that exactly that.
+
+## Features
+
+- Generate a Todoist task directly in your Todoist Inbox from Joplin.
+- Automatically adds a link in your current Joplin note to the Todoist task.
+- Automatically add a link to the current Joplin note in the Todoist task description.
+
+## Setup
+
+1. Navigate to your [Todoist settings](https://app.todoist.com/app/settings/integrations/developer) and copy your **API token**.
+
+1. In Joplin, go to **Tools > Options > Joplin to Todoist** and past the your Todoist API token.
+
+1. Click on **OK** to apply the changes.
+
+## Create a task
+
+1. Open the note you want to add a task from
+1. Click on the new task icon
+
+    ![new-task-button](/screenshots/add-task-button.png)
+
+1. Fill in the task information
+
+    ![new-task-dialog](/screenshots/new-task-dialog.png)
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,111 @@
 import joplin from 'api';
+import { MenuItemLocation, ToolbarButtonLocation, SettingItemType} from 'api/types';
+import { Project, TodoistApi } from "@doist/todoist-api-typescript"
+
 
 joplin.plugins.register({
 	onStart: async function() {
+		
+		const dialogs = joplin.views.dialogs;
+
+		// Create a "new todoist task" dialog to collect input from user
+		const taskDialog = await dialogs.create('myDialog3');
+
+		// Add CSS styling to the "new todoist task" dialog
+		await joplin.views.dialogs.addScript(taskDialog,'./taskDialog.css');
+		
+
 		// eslint-disable-next-line no-console
-		console.info('Hello world. Test plugin started!');
+		console.info('Joplin to Todist plugin v 0.2 started');
+		console.info('Registering Todoist API');
+
+		// Create a section in settings called "Todoist to Joplin"
+		await joplin.settings.registerSection('ttmSection', {
+            label: 'Joplin to Todoist',
+            iconName: 'fas fa-list',
+        });
+
+		// Add a input box in the "Todoist to Joplin" section in settings
+		// so the user can enter their Todoist API key
+		await joplin.settings.registerSettings({
+            'ttmKeySetting': {
+                value: '',
+                type: SettingItemType.String,
+                section: 'ttmSection',
+                public: true,
+                secure: false,
+                label: 'Todoist API token',
+            },
+        });
+		
+		
+		await joplin.commands.register({
+			name: 'newTodoistTask',
+			label: 'New Todoist Task',
+			iconName: 'fas fa-clipboard-check',
+			execute: async () => {
+				const key = await joplin.settings.value('ttmKeySetting');
+				const api = new TodoistApi(key);
+                if (!key) 
+				{
+                    console.error('ToDoist API token not found.');
+					alert("No API token found for Todoist. Please enter a token in the settings.")
+                } 
+				else 
+				{
+					//Get selected text
+					var selectedText = (await joplin.commands.execute('selectedText') as string);
+
+					var note = await joplin.workspace.selectedNote();
+					console.dir(note);
+
+					await dialogs.setHtml(taskDialog, `
+					<form name="taskInfo">
+					<b>Task Title</b> <input type="text" name="taskname" value="`+ selectedText +`" />
+					<br />
+					<b>Task Due</b> <input type="text" name="due" placeholder="(ex: 'Today', 'Friday', 'Every day')"/>
+					<br />
+					<b>Task Description</b>
+					<textarea name="desc" placeholder="Description for your task. A link to the currently selected Joplin note will be appended here."></textarea>
+				  	</form>
+					`);
+
+					const taskDialogResults = await dialogs.open(taskDialog);
+					console.dir(taskDialogResults);
+
+					var taskdata = {
+						content: taskDialogResults.formData.taskInfo.taskname,
+						dueString: taskDialogResults.formData.taskInfo.due,
+						description: taskDialogResults.formData.taskInfo.desc + "\n" + "Joplin Note: [" + note.title + "](joplin://x-callback-url/openNote?id=" + note.id + ")" ,
+						dueLang: "en",
+						priority: 4
+					};
+
+					console.info(taskdata);
+
+					api.addTask(taskdata).then((task) => {
+						console.log(task);
+						console.log(task.url);
+						updateNoteBody("- [" + task.content + "](" + task.url + ")");
+						
+					}).catch((error) => console.log(error))
+				}			
+			},
+		});
+
+		// Add the first command to the note toolbar
+		await joplin.views.toolbarButtons.create('newTodoistTaskBTTN', 'newTodoistTask', ToolbarButtonLocation.NoteToolbar);
 	},
 });
+
+export async function updateNoteBody(newBodyStr: string) 
+{
+    //console.info("Update note: " + noteId);
+	
+	
+	await joplin.commands.execute('textSelectAll');
+	const current = await joplin.commands.execute("selectedText");
+	await joplin.commands.execute("replaceSelection", current + "\n\n" + newBodyStr);
+	  
+}
+
